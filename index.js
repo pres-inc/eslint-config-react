fdabb24e233d6d0579cbc3e6bbbea171ddefe456
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-31 03:28:40 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "plugins": [
+    "react",
+    "react-hooks"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "jsx-quotes": [
+      "error",
+      "prefer-double"
+    ],
+    "react/hook-use-state": "error",
+    "react/no-array-index-key": "error",
+    "react/jsx-boolean-value": "error",
+    "react/jsx-closing-bracket-location": "error",
+    "react/jsx-curly-brace-presence": [
+      "error",
+      {
+        "props": "never",
+        "children": "never"
+      }
+    ]
+  }
+}
